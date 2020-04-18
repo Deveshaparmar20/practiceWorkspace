246f64e42d21efcf570f542a1329eb9e3a62f18c
Deveshaparmar20 <63926363+Deveshaparmar20@users.noreply.github.com>
2020-04-19 00:21:09 +0530

    Add files via upload

---
+++ LinkedListProj/InBuiltLinkedList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListNewImp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList list = new MyList();
+            //list.AddToEnd(9);
+            //list.AddToEnd(12);
+            //list.AddToEnd(15);
+            //list.AddToEnd(1);
+            //list.AddToBeginning(9);
+            //list.AddToBeginning(12);
+            //list.AddToBeginning(15);
+            //list.AddToBeginning(1);
+            list.AddSorted(9);
+            list.AddSorted(12);
+            list.AddSorted(15);
+            list.AddSorted(1);
+            list.Print();
+            Console.ReadKey();
+        }
+    }
+}
