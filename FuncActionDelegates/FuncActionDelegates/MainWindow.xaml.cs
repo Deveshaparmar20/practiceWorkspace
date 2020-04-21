18e99d2ce44119eccdb420fa873df2ec11b68b51
Devesha Parmar <devesha.parmar@honeywell.com>
2020-04-21 18:29:53 +0530

    Delegates

---
+++ FuncActionDelegates/FuncActionDelegates/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace FuncActionDelegates
+{
+    public partial class MainWindow : Window
+    {
+        PersonFormat formatPerson;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PersonListBox.ItemsSource = People.GetPeople();
+        }
+
+        public void AssignDelegate()
+        {
+            if (Option1Button.IsChecked.Value)
+                formatPerson = Formatters.Default;
+            else if (Option2Button.IsChecked.Value)
+                formatPerson = Formatters.LastNameToUpper;
+            else if (Option3Button.IsChecked.Value)
+                formatPerson = Formatters.FirstNameToLower;
+            else if (Option4Button.IsChecked.Value)
+                formatPerson = Formatters.FullName;
+        }
+
+        private void ProcessDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            OutputList.Items.Clear();
+            AssignDelegate();
+            foreach (Person person in PersonListBox.Items)
+                OutputList.Items.Add(person.ToString(formatPerson));
+        }
+    }
+}
