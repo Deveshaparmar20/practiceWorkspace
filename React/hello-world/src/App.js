46e1f78517152a39f10905876dcf9a975af707eb
Devesha Parmar <devesha.parmar@honeywell.com>
2020-05-18 04:16:12 +0530

    render props

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -10,13 +10,20 @@ import Hero from './components/Hero';
 import ErrorBoundary from './components/ErrorBoundary';
 import ClickCounter from './components/ClickCounter';
 import HoverCounter from './components/HoverCounter';
+import ClickCounterTwo from './components/ClickCounterTwo';
+import HoverCounterTwo from './components/HoverCounterTwo';
+import User from './components/User';
+import CounterA from './components/CounterA';
 
 class App extends Component {
   render() {
     return (
       <div className="App"> 
-      <HoverCounter/>
-      <ClickCounter name='devesha'/>
+      <CounterA render={(count,incrementCount)=><ClickCounterTwo count={count} incrementCount={incrementCount}/>}/>
+      <CounterA render={(count,incrementCount)=><HoverCounterTwo count={count} incrementCount={incrementCount}/>}/>
+
+      {/* <HoverCounter/>
+      <ClickCounter name='devesha'/> */}
       {/* <ErrorBoundary>
       <Hero heroName="Batman"/>
       <Hero heroName="Superman"/>
