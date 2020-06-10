9690ab351f80b9ac5a84748b8ff08843c4e5dc7d
Devesha Parmar <devesha.parmar@honeywell.com>
2020-06-11 01:32:48 +0530

    C# design patterns

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton obj1 = Singleton.Instance;
+
+            Singleton obj2 = Singleton.Instance;
+
+            Console.ReadLine();
+        }
+    }
+}
